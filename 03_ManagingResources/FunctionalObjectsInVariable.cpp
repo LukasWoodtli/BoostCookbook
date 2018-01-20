a24a42f4fa364449103316fe4908c2a67869653d
Lukas Woodtli <woodtli.lukas@gmail.com>
2018-01-20 19:32:05 +0100

    Add example for function pointers in boost::function
    - also for std::function
    - fixed a bug wit std::for_each and arrays

--- 03_ManagingResources/FunctionalObjectsInVariable.cpp
+++ 03_ManagingResources/FunctionalObjectsInVariable.cpp
@@ -2,9 +2,14 @@
 #include <iostream>
 #include <boost/function.hpp>
 
+
 ////////////////////////////////////////////////////////////////
+/// Storing any functional objects in a variable ///////////////
+////////////////////////////////////////////////////////////////
+
+
 // boost style /////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
+
 class int_processor {
   const int min_;
   const int max_;
@@ -18,7 +23,7 @@ public:
     {}
 
   void operator()(int i) const {
-    if (i < min_ || i > max_) {
+    if (i < min_ || i >= max_) {
       triggered_ = true;
     }
   }
@@ -28,7 +33,8 @@ typedef boost::function<void(int)> fobject_t;
 
 void process_integers(const fobject_t& f) {
     static const int data[] = {1, 2, 3, 4, 5, 200};
-    std::for_each(data, data + sizeof(data), f);
+    auto size = sizeof(data); (void)size;
+    std::for_each(data, data + sizeof(data)/sizeof(*data), f);
 }
 
 // boost::function is convertible to bool
@@ -42,9 +48,9 @@ bool foo(const fobject_t& f) {
   }
 }
 
-////////////////////////////////////////////////////////////////
+
 // C++ 11 style ////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
+
 class int_processor_std {
   const int min_;
   const int max_;
@@ -58,7 +64,7 @@ public:
     {}
 
   void operator()(int i) const {
-    if (i < min_ || i > max_) {
+    if (i < min_ || i >= max_) {
       triggered_ = true;
     }
   }
@@ -69,7 +75,7 @@ typedef std::function<void(int)> stdfobject_t;
 void process_integers_std(const stdfobject_t& f) {
     if (not f) exit(EXIT_FAILURE);
     static const int data[] = {1, 2, 3, 4, 5, 200};
-    std::for_each(data, data + sizeof(data), f);
+    std::for_each(data, data + sizeof(data)/sizeof(*data), f);
 }
 
 // std::function is convertible to bool
@@ -84,18 +90,27 @@ bool foo_std(const stdfobject_t& f) {
 }
 
 
+////////////////////////////////////////////////////////////////
+// Passing a function pointer in a variable ////////////////////
+////////////////////////////////////////////////////////////////
+
+void my_ints_function(int i) {
+    std::cout << __FUNCTION__ << " called: int is " << i << "\n";
+}
+
+////////////////////////////////////////////////////////////////
+// Tests ///////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
 #define ASSERT(b) do {if(not b) {std::cout << "ASSERT failed\n"; exit(EXIT_FAILURE);}}while(0)
 
 int main() {
+/// Storing any functional objects in a variable ///////////////
   // boost
   bool is_triggered = false;
   int_processor fo(0, 200, is_triggered);
   process_integers(fo);
-
-#if !(defined(__linux__) && defined(__GNUC__))
-  // this is not working on travis Linux/GCC
   ASSERT(is_triggered);
-#endif
 
   // boost::function to bool conversion
   fobject_t empty;
@@ -116,4 +131,15 @@ int main() {
   ASSERT(not result);
   result = foo(fo_std);
   ASSERT(result);
+
+
+// Passing a function pointer in a variable ////////////////////
+  std::cout << "\nFunction pointer:\n";
+  // boost
+  std::cout << "boost:\n";
+  process_integers(&my_ints_function);
+  // C++11
+  std::cout << "C++11 (std):\n";
+  process_integers_std(&my_ints_function);
+
 }
--- README.md
+++ README.md
@@ -49,8 +49,8 @@ Examples for ["Boost C++ Application Development Cookbook"](https://www.packtpub
 | Reference counting of pointers to classes used across methods | 74   |   ✓  |
 | Managing pointers to arrays that do not leave scope           | 77   |   ✓  |
 | Reference counting pointers to arrays used across methods     | 79   |   ✓  |
-| Storing any functional objects in a variable                  | 82   |      |
-| Passing a function pointer in a variable                      | 85   |      |
+| Storing any functional objects in a variable                  | 82   |   ✓  |
+| Passing a function pointer in a variable                      | 85   |   ✓  |
 | Passing C++11 lambda functions in a variable                  | 86   |      |
 | Containers of pointers                                        | 88   |      |
 | Doing something at scope exit                                 | 91   |      |
